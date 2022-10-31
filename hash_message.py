e7858304c15e15168b0bf656440f9dc00fda200c
nranisnjwa <114972542+nranisnjwa@users.noreply.github.com>
2022-10-31 13:06:30 +0800

    Update hash_message.py

--- hash_message.py
+++ hash_message.py
@@ -1,9 +1,7 @@
-# This file signs a file with the owner's private key and
-# verifies the signature with the owners public key
 import rsa
 
 
-# Open key file and return key data
+#Open key file and return key data
 def file_open(file):
     key_file = open(file, 'rb')
     key_data = key_file.read()
@@ -11,17 +9,17 @@ def file_open(file):
     return key_data
 
 
-# Open private key file and load in key
+#Open private key file
 privkey = rsa.PrivateKey.load_pkcs1(file_open('privatekey.key'))
 
-# Open the secret message file and return data to variable
-pdf = file_open('try.pdf')
-hash_value = rsa.compute_hash(pdf, 'SHA-512')  # optional
+
+myfile = file_open('try.pdf')
+hash_value = rsa.compute_hash(myfile, 'SHA-512')  
 
 
 
-# Sign the message with the owners private key
-signature = rsa.sign(pdf, privkey, 'SHA-512')
+#Sign the message with the sender private key
+signature = rsa.sign(myfile, privkey, 'SHA-512')
 
 s = open('signature_file','wb')
 s.write(signature)
@@ -30,5 +28,5 @@ s.write(signature)
 print(signature)
 print(len(signature))
 
-print(len(hash_value) * 8)  # to verify size of hash/output
+print(len(hash_value) * 8) 
 
