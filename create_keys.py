ee948963f2aa1b8dfa8a835c6adf8637f11f2f5f
nranisnjwa <114972542+nranisnjwa@users.noreply.github.com>
2022-10-31 12:56:25 +0800

    Update create_keys.py

--- create_keys.py
+++ create_keys.py
@@ -1,12 +1,14 @@
 import rsa
 
-# create the pub & private keys
+#create the pub & private keys
 (pubkey, privkey) = rsa.newkeys(2048)
 
-# write the public key to a file
+#write the public key to a file
 with open('publickey.key', 'wb') as key_file:
     key_file.write(pubkey.save_pkcs1('PEM'))
+    print("Public Key was created")
 
-# write the private key to a file
+#write the private key to a file
 with open('privatekey.key', 'wb') as key_file:
     key_file.write(privkey.save_pkcs1('PEM'))
+    print("Private Key was created")
