47d02b064d5d44931adab2f512a73406fa09ce0d
nranisnjwa <114972542+nranisnjwa@users.noreply.github.com>
2022-10-31 12:58:05 +0800

    Update decrypt_all_file.py

--- decrypt_all_file.py
+++ decrypt_all_file.py
@@ -1,6 +1,6 @@
 import rsa
 
-# Open key file and return key data
+#Open key file and return key data
 def file_open(file):
     key_file = open(file, 'rb')
     key_data = key_file.read()
@@ -8,13 +8,13 @@ def file_open(file):
     return key_data
 
 
-# Open public key file and load in key
+#Open public key file
 pubkey = rsa.PublicKey.load_pkcs1(file_open('publickey.key'))
 
 pdf = file_open('try.pdf')
 signature = file_open('signature_file')
 
-# Verify the signature to show if successful or failed
+#Verify the signature to show if successful or failed
 try:
     rsa.verify(pdf,signature,pubkey)
     print("Signature successfully verified")
